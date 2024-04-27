d3ca1d6c3e2366f3305b12aa36f9102f6d0c97e1
Shimshir Emirdzan IU7-23B <shimshireao@student.bmstu.ru>
2024-04-27 19:53:07 +0300

    add typeWhileChannelOpen

--- internal/tesseract/tessercat.go
+++ internal/tesseract/tessercat.go
@@ -2,62 +2,65 @@ package tesseract
 
 import (
 	"github.com/EmirShimshir/tasker-bot/internal/config"
+	"github.com/otiai10/gosseract/v2"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
 )
 
 type Nlp struct {
-	//client    *gosseract.Client
-	//languages []string
+	client    *gosseract.Client
+	languages []string
 }
 
 func NewNlp(cfg config.Tesseract) *Nlp {
 	return &Nlp{
-		//client:    gosseract.NewClient(),
-		//languages: cfg.NlpLanguages,
+		client:    gosseract.NewClient(),
+		languages: cfg.NlpLanguages,
 	}
 }
 
 func (n *Nlp) TextRecognition(urlFile string) (string, error) {
-	//resp, err := http.Get(urlFile)
-	//if err != nil {
-	//	return "", err
-	//}
-	//defer resp.Body.Close()
-	//
-	//file, err := os.CreateTemp("", "")
-	//if err != nil {
-	//	return "", err
-	//}
-	//defer file.Close()
-	//defer os.Remove(file.Name())
-	//
-	//_, err = io.Copy(file, resp.Body)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//err = n.client.SetLanguage(n.languages...)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//err = n.client.SetImage(file.Name())
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//text, err := n.client.Text()
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//return text, nil
+	resp, err := http.Get(urlFile)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
-	return urlFile, nil
+	file, err := os.CreateTemp("", "")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	defer os.Remove(file.Name())
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	err = n.client.SetLanguage(n.languages...)
+	if err != nil {
+		return "", err
+	}
+
+	err = n.client.SetImage(file.Name())
+	if err != nil {
+		return "", err
+	}
+
+	text, err := n.client.Text()
+	if err != nil {
+		return "", err
+	}
+
+	return text, nil
 }
 
 func (n *Nlp) CloseClient() {
-	//err := n.client.Close()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	err := n.client.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
